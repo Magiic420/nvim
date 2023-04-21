cd88f626c557e8bdfa3373b8f96fb4e5440b877c
Magiic420 <122331591+Magiic420@users.noreply.github.com>
2023-04-21 15:16:48 -0400

    Update terminal.lua

--- lua/Magic/commands/terminal.lua
+++ lua/Magic/commands/terminal.lua
@@ -4,9 +4,6 @@ local function terminal()
     vim.cmd('buffer #')
 
     vim.fn.termopen('bash')
-    -- local bufnr = vim.api.nvim_get_current_buf()
-
-    -- vim.api.nvim_buf_set_option(bufnr, 'modifiable', true)
 end
 
 vim.api.nvim_create_user_command("NewTerminal",
